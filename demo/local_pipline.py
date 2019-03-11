769ac9c90636b795a76f282e20bb484a2317d6b3
zyanyway <zyanyway@gmail.com>
2019-03-11 19:30:54 +1100

    add some annotation

--- demo/local_pipline.py
+++ demo/local_pipline.py
@@ -1,16 +1,24 @@
+"""
+python version: 2.7
+
+data processing:  read data from local sqlite -> write the data to gbq
+locally run the data processing using API of google cloud
+
+"""
+
 from __future__ import print_function
 
-import apache_beam as beam
 from google.cloud import bigquery
 import pandas as pd
 import sqlite3
 import os
 import logging
-from apache_beam.options.pipeline_options import PipelineOptions
 
 
 def read_data():
-
+'''
+using pandas to read the local sqlite
+'''
     conn = sqlite3.connect(
         "./db/run_5b7252f014da39f6cabbc5f8_cad2e964-2e6c-4dfc-8818-e4bf7b42b816_results_dbs_global_merge_output_block.db")
     df = pd.read_sql_query(
@@ -21,6 +29,9 @@ def read_data():
 
 
 def bq_create_dataset():
+'''
+if there is no dataset with assigned id in the google bigquery, create it
+'''
     bigquery_client = bigquery.Client()
     dataset_ref = bigquery_client.dataset('my_dataset_id')
 
@@ -28,13 +39,16 @@ def bq_create_dataset():
         bigquery_client.get_dataset(dataset_ref)
 
     except Exception as e:
-        print(e)
+        # print(e)
         dataset = bigquery.Dataset(dataset_ref)
         dataset = bigquery_client.create_dataset(dataset)
         print('Dataset {} created.'.format(dataset.dataset_id))
 
 
 def write_bq(data):
+'''
+write data to bigquery, using the API of pandas. The to_gbq can create the table automatically according Dataframe column labels info
+'''
     bq_create_dataset()
     data.to_gbq('my_dataset_id.my_table',
                    'demo2zy', if_exists='replace')
